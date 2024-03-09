36fc3a5235de73178e6347440a4dc218b4a7b22a
beaubouchard <bnb2011@gmail.com>
2024-03-09 16:46:23 -0500

    feat: adding 5 base rituals

--- src/models/rites.ts
+++ src/models/rites.ts
@@ -16,7 +16,13 @@ interface RiteSlackResponse {
   text: string;
 }
 
-const riteList = ["You will make a rite with your friend."];
+const riteList = [
+  "Exercise: Stand in front of the mirror and look directly into your refelction's eyes. Say 'I love you'. Take a deep breath, and deep exhale. Repeat 3 times.",
+  "Exercise: Regular meditation is a definite way to streangthen your connection to your intuition. Try to meditate for 5 minutes today.",
+  "Exercise: Write down 3 things you are grateful for. Keep the list in your pocket and read it whenever you feel down.",
+  "Exercise: Take a walk in nature and take a moment to appreciate the beauty around you.",
+  "Exercise: Take a moment to appreciate the beauty of the sky. Look up and take a deep breath.",
+];
 
 function getRite(): string {
   return riteList[Math.floor(Math.random() * riteList.length)];
